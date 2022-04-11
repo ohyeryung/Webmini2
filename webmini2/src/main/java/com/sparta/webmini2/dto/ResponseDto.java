56a1b814108657d3d681db7c54f3b6df63115ae3
ohyeryung <100130070+ohyeryung@users.noreply.github.com>
2022-04-11 17:40:49 +0900

    주석 추가

--- webmini2/src/main/java/com/sparta/webmini2/dto/ResponseDto.java
+++ webmini2/src/main/java/com/sparta/webmini2/dto/ResponseDto.java
@@ -14,12 +14,13 @@ public class ResponseDto {
         this.result = result;
     }
 
-
+    // 회원가입 response
     public ResponseDto(boolean result, String errormessage) {
         this.result = result;
         this.errormessage = errormessage;
     }
 
+    // 로그인 여부 확인 response
     public ResponseDto(String username, String userNickName, String userPosition) {
         this.username = username;
         this.nickName = userNickName;
