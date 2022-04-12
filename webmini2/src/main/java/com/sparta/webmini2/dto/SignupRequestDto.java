ad53cc14bdd776414348bdf757f05e75c71dd5c7
blingbc9 <blingbc@gmail.com>
2022-04-12 10:13:06 +0900

    test

--- webmini2/src/main/java/com/sparta/webmini2/dto/SignupRequestDto.java
+++ webmini2/src/main/java/com/sparta/webmini2/dto/SignupRequestDto.java
@@ -1,12 +1,22 @@
-package com.sparta.webmini2.dto;
+package com.sparta.webmini2.Dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 public class SignupRequestDto {
     private String username;
-    private String nickName;
     private String password;
     private String passwordCheck;
+    private String nickName;
     private String position;
+
+    public SignupRequestDto(String username, String password, String passwordCheck,String nickName,String position) {
+        this.username = username;
+        this.password = password;
+        this.passwordCheck = passwordCheck;
+        this.nickName = nickName;
+        this.position = position;
+    }
 }
